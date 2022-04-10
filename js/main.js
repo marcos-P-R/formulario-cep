2b647b2bd0c659bba3060c4c0062fb5e60bce76d
Marcos Paulo R R <marcos.p.mcruz@outlook.com>
2022-04-10 16:38:54 -0300

    Adicionando os demais campos do formulario

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 window.addEventListener('load', () => {
-    const inputCep = document.querySelector('#cep')
-    receberCep(inputCep)
+    const inputCep = document.querySelector('#cep');
+    receberCep(inputCep);
 });
 
 const receberCep = (input) => {
     const apiCep = addEventListener('focusout', () => {
-        const cep = input.value
-        getCepApi(cep)
+        const cep = input.value;
+        getCepApi(cep);
     });
 }
 
 async function getCepApi(cep) {
-    const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    const data = await request.json()
-    const inputCidade = document.querySelector("#cidade")
-    const inputEstado = document.querySelector("#estado")
+    const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await request.json();
+    const inputCidade = document.querySelector("#cidade");
+    const inputEstado = document.querySelector("#estado");
+    const inputBairro = document.querySelector("#bairro");
+    const inputComplemento = document.querySelector("#complemento");
+    const inputLogradouro = document.querySelector("#logradouro");
 
-    inputCidade.value = data.localidade
-    inputEstado.value = data.uf
-
+    inputCidade.value = data.localidade;
+    inputEstado.value = data.uf;
+    inputBairro.value = data.bairro;
+    inputComplemento.value = data.complemento;
+    inputLogradouro.value = data.logradouro;
 }
